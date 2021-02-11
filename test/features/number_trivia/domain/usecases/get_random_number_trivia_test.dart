23c074ba152278cb8f1f4ad62d0298b49c3dfce4
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2021-02-11 11:03:24 +0200

    refactor domain layer

--- test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:tdd_number_trivia/core/usecase/usecase.dart';
 import 'package:tdd_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:tdd_number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:tdd_number_trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
@@ -30,7 +31,7 @@ void main() {
           .thenAnswer((_) async => Right(tNumberTrivia));
 
       // act
-      final result = await usecase.execute();
+      final result = await usecase.call(NoParams());
 
       // assert
       expect(result, Right(tNumberTrivia));
